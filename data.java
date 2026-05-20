a551714de2489b583da754d288c1c9e8f846ac89
sreeja <sreejanuneti25@gmail.com>
2026-05-20 12:22:43 +0000

    New added

--- data.java
+++ data.java
@@ -1,6 +1,6 @@
 public static void main(String[] args) {
     System.out.println("Hello, World!");   
-    System.out.println("Welcome to Java programming.");
-                          
+    System.out.println("Welcome to Java programming."); 
+    System.err.println();            
 
 }
